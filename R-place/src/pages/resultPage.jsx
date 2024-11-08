b243121d34f643fa70522b8650c8b17850e22c6b
Tomoki Yoshizawa <tomoki-goalie@hotmail.com>
2024-11-08 01:14:04 -0800

    change

--- R-place/src/pages/resultPage.jsx
+++ R-place/src/pages/resultPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./ResultPage.scss";
+import "./resultPage.scss";
 import emailjs from "emailjs-com";
 
 function ResultPage() {
